af0a190aebb3653ecd218332ead9143b766470f8
Nir Galon <nir@galons.io>
2024-07-24 09:58:51 +0300

    fix: add posthog

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: false },
   modules: ["@nuxt/ui", "@nuxt/content", "@nuxt/fonts", "@nuxthq/studio"],
+  runtimeConfig: {
+    public: {
+      posthogPublicKey: "phc_jG8dJmO91LE2PuTEh8fe7OzD2NXryjyfIju9LiHTwth",
+      posthogHost: "https://us.i.posthog.com",
+    },
+  },
   fonts: {
     defaults: {
       weights: [200, 400, 500, 600, 700, 800],
